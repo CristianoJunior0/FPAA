371da88f8b051de8c09d0b9950b1e56598489f09
Pires, Cristiano Nunes <cristiano.nunes@arcelormittal.com.br>
2025-08-27 13:57:02 -0300

    modified: karatsuba.py

--- karatsuba.py
+++ karatsuba.py
@@ -4,20 +4,11 @@ def karatsuba(x, y):
 
     n = max(len(str(x)), len(str(y)))
     m = n // 2
+    a, b = divmod(x, 10**m)
+    c, d = divmod(y, 10**m)
 
-    a = x // 10**m
-    b = x % 10**m
-    c = y // 10**m
-    d = y % 10**m
+    ac = karatsuba(a, c)
+    bd = karatsuba(b, d)
+    ab_cd = karatsuba(a + b, c + d)
 
-    z0 = karatsuba(b, d)
-    z1 = karatsuba(a + b, c + d)
-    z2 = karatsuba(a, c)
-
-    return (z2 * 10**(2*m)) + ((z1 - z2 - z0) * 10**m) + z0
-
-
-if __name__ == "__main__":
-    x = int(input("Digite o primeiro número: "))
-    y = int(input("Digite o segundo número: "))
-    print("Resultado:", karatsuba(x, y))
+    return ac * 10**(2*m) + (ab_cd - ac - bd) * 10**m + bd
